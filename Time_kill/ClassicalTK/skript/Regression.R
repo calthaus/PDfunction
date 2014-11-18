a6f49f7f2e22959a0429e257fae51c85f336ac39
sunnivas <sfoerster@ispm.unibe.ch>
2014-11-18 10:57:49 +0100

    Time-kill experiments with improved plotting

--- Time_kill/ClassicalTK/skript/Regression.R
+++ Time_kill/ClassicalTK/skript/Regression.R
@@ -1,17 +1,16 @@
 ###################################################################################################################################
-##################################fit pharmacodynamic function and impute missing data#############################################
+##################################fit linear regressions and plot lines############################################################
 ###################################################################################################################################
 
 #set working directory  
-setwd("C:/Users/sfoerster/ABinteractions/Time_kill/GrowthRate")
+setwd("C:/Users/sfoerster/ABinteractions/Time_kill/ClassicalTK")
 options(scipen=1)
+
 # Negative log-likelihood
 nll <- function(a, b, sigma, data = data2regress) {
   m <- a + b * data$x
-  m <- ifelse(m == 0, 1e-5, m) # to avoid divisions by 0 (not really clean, but this should happen very rarely)
-  #v <- (sigma / m)^2           # to avoid issues if m < 0 in dnorm, pnorm calls below  
-  #v <- (sigma)^2              # constant error (additive on log scale)
-  v <- (sigma/data$y_cens)^2             # constant error (additive on log scale)
+  m <- ifelse(m == 0, 1e-5, m) 
+  v <- (sigma/data$y_cens)^2            
   ll_i <- (1 - data$cens) * dnorm(data$y_cens, mean = m, sd = sqrt(v), log = TRUE) + 
   data$cens * pnorm(data$y_cens, mean = m, sd = sqrt(v), log.p = TRUE) 
   ll <- sum(ll_i)
@@ -22,6 +21,8 @@ nll <- function(a, b, sigma, data = data2regress) {
 library("drc")
 library("XLConnect")
 library("bbmle")
+library("drc")
+
 #initialize some empty vectors and empty variables
 parmlist=vector()
 append2=vector()
@@ -43,13 +44,13 @@ mypalette<- c("red","lightblue","lightblue","lightblue","#C6DBEF", "#9ECAE1" ,"#
 # list of all files and pattern to be recognized in these files
 files <- list.files(path =sourcefiledir, pattern = sourcefilepattern, all.files = FALSE, recursive = FALSE, ignore.case = FALSE, include.dirs = FALSE)
 ablist<-c("WHOA_AZ","WHOA_CIP","WHOA_Tet","WHOA_CT","F89_AZ","F89_CIP","F89_Tet","F89_CT")
-#titellist <- c("WHOA Azithromycin","WHOA Ciprofloxacin","WHOA Tetracyclin","WHOA Ceftriaxone","F89 Azithromycin","F89 Ciprofloxacin","F89 Tetracyclin","F89 Ceftriaxone")
 
 #do action for the dataset with the recognized pattern
 for (ab in ablist){
   replicatespattern=ab
   replicates <- list.files(path =sourcefiledir, pattern = ab, all.files = FALSE, recursive = FALSE, ignore.case = FALSE, include.dirs = FALSE)
   print(replicates)
+  
   #read data from file with the recognized pattern and append to a dataframe
   for (replicate in replicates){
     append1 <- read.table(paste(verzeichnis,"/",replicate,sep=""),header=TRUE)
@@ -60,28 +61,14 @@ for (ab in ablist){
 }
 
 #remove values when already at timepoint 0 no colonies couldnt be counted anymore
-# mydata1 <- subset(mydata,mydata$hours==0)
-# mydata1 <- subset(mydata1,mydata1$count>0)
-# mydata2 <- subset(mydata,hours>0)
-# mydata <- rbind(mydata1,mydata2)
-#mean
-# mydata1<-subset(mydata,mydata$hours==0)
-# mean0 <- mean(mydata1$count)
-# mydata1$count<-mean0
-# mydata1 <- subset(mydata1,mydata1$count>0)
-# mydata2 <- subset(mydata,hours>0)
-# mydata <- rbind(mydata1,mydata2)
 problem<-subset(mydata,mydata$hours==0&mydata$count==0)
 rows_to_garbage1 = which(mydata$sample==toString(problem$sample[1]), useNames = TRUE)
 mydata <- mydata[-c(rows_to_garbage1),]
 rows_to_garbage2 = which(mydata$sample==toString(problem$sample[2]), useNames = TRUE)
 mydata <- mydata[-c(rows_to_garbage2),]
 
-print(rows_to_garbage1)
-print(rows_to_garbage2)
 
 for(j in unique(mydata$replicate)){
-  print("----------------------------filename------------------------------------------")
   data=mydata[mydata$replicate==j,]
   name=sub(".txt","regression.pdf",j)
   pdf(paste(name,sep=""))
@@ -90,42 +77,30 @@ for(j in unique(mydata$replicate)){
   hours2=data$hours
   CFU=data$count*data$dilution*100
   CFU2=data$count*data$dilution*100
-  #CFU[CFU==0&time==0]<- NA
-  #cens=CFU
-  #cens[cens==0]<-1
-  #cens[cens>1]<-0
   CFU[CFU==0]<-100
-  antibiotic=round(data$MIC,3)
+  antibiotic=data$antibiotic.conc
   ab.conc<-factor(antibiotic)
+  options(scipen=-1000)
   conc=levels(ab.conc)
   conc[conc==min(conc)]= 0
-  
-  #dat=data.frame(cbind(x=time, cens=cens, y_cens=CFU,ab=antibiotic))
-  #dat[, c('x', 'cens', 'y_cens')]
+  conc=as.vector(round((as.numeric(conc)),3))
   tmin<-0 
   tmax<-6 
   # define a vector for the decline rates:
   slopes<-vector("numeric",length=nlevels(ab.conc))
-  #Calculate the linear regressions
- # for(idose in 1:nlevels(ab.conc)){
-  #  dose<-as.numeric(levels(ab.conc)[idose])
-   # data2regress<-data.frame(time=hours[hours>=tmin & hours<=tmax &antibiotic==dose],log.bacteria.rel=log(CFU[hours>=tmin&hours<=tmax & antibiotic==dose]/CFU[hours==tmin&antibiotic==dose]))
-    #slopes[idose]<-coef(lm(log.bacteria.rel~time-1,data2regress))
-  #}
-  
-  #start values a=log(CFU) at time 0, b=0
-  slopes<-vector()
-  #par(mar=c(5,5,1,1))
-  #plot(c(-2,6),c(10,1e9),type="n",xlab="Time [h]",main=j,log="y",ylab="Bacteria [CFU/ml]",cex.lab=1.3,cex=1.4,lty=2,lwd=8)
-  library("drc")
+
+
   plot(c(-2,6),c(10,1e10),type="n",xlab="Time [h]",log="y",ylab="Bacteria [CFU/ml]",cex.lab=1.3,cex=1.4,lty=2,lwd=8)
-  legend(x=3,y=100,"Limit of detection: 100 CFU/ml", bty="n",text.col="gray47",cex=0.8)
-  legend("topleft",conc,col=mypalette,bty="n",text.col="gray47",pch=1,cex=0.8,title="conc.[mg/L]")
+  legend(x=3,y=100,"Limit of detection: 100 CFU/ml", bty="n",cex=0.8)
+  options(scipen=1)
+
+  legend("topleft",legend=conc,col=mypalette,bty="n",pch=1,cex=0.8,title="conc.[mg/L]")
   axis.break(axis=1,breakpos=-1,style="slash") 
-  abline(h =100, untf = FALSE,col="gray47",lty=2)
+  abline(h =100, untf = FALSE,lty=2)
+  
   for(idose in 1:nlevels(ab.conc)){
     dose<-as.numeric(levels(ab.conc)[idose])
-    #data2regress=data.frame(cbind(x=time, cens=cens, y_cens=CFU,ab=dose)) 
+
     cens=CFU[hours>=tmin&hours<=tmax & antibiotic==dose]
     y_lines=CFU[hours>=-2&hours<=6 & antibiotic==dose]
     x_lines=hours[hours>=-2&hours<=6 & antibiotic==dose]    
@@ -139,24 +114,15 @@ for(j in unique(mydata$replicate)){
     cens[cens==0]<-1
     cens[cens>1]<-0
     y_cens=log(CFU[hours>=tmin & hours<=tmax &antibiotic==dose])
-    #data2regress<-data.frame(x=hours[hours>=tmin & hours<=tmax &antibiotic==dose],y_cens=log(CFU[hours>=tmin&hours<=tmax & antibiotic==dose]/CFU[hours==tmin&antibiotic==dose]),cens=cens)
     data2points<-data.frame(x=hours[hours>=tmin & hours<=tmax &antibiotic==dose],y_cens=CFU[hours>=tmin & hours<=tmax &antibiotic==dose],cens=cens)
     data2regress<-data.frame(x=hours[hours>=tmin & hours<=tmax &antibiotic==dose],y_cens=log(CFU[hours>=tmin & hours<=tmax &antibiotic==dose]),cens=cens)
-    #data2lines<-data.frame(x_lines=hours[hours>=-2 & hours<=6 &antibiotic==dose],y_lines=CFU[hours>=-2 & hours<=6 &antibiotic==dose],cens=cens)
-    
-    #names(data2regress)<-c("x")
-    print(data2regress)
-    #slopes[idose]<-coef(lm(log.bacteria.rel~time-1,data2regress))
-    #fit <- mle2(nll, start=list(a =log(1000000), b=-5, sigma = 1), method="Nelder-Mead", control=list(maxit=1e3))
+
     fit <- mle2(nll, start=list(a =log(1000000), b=-1,sigma=0.1), method="Nelder-Mead", control=list(maxit=1e3))
     
-    print("---------------------------------------------------------------------------------------------------")
     print(fit)
     b <- coef(fit)[[2]]
     fita <- c(coef(fit)[[1]],coef(fit)[[2]])
     predict<-coef(fit)[[1]]+data2regress$x*coef(fit)[[2]]  
-    #points(data2points$x,data2points$y_cens,col=mypalette[idose],pch=1,cex=1.5)
-    #lines(data2lines$x,data2lines$y,col=mypalette[idose],pch=1,cex=1.5,lty=2)
     lines(x_lines,y_lines,col=mypalette[idose],lty=1) 
     points(x_points,y_points,col=mypalette[idose],lty=1, pch=1, cex=1.5)
     slopes <- c(fita,slopes)
--- Time_kill/GrowthCurves/skript/growthcurves_paper.R
+++ Time_kill/GrowthCurves/skript/growthcurves_paper.R
@@ -1,47 +1,92 @@
-### Fitting Gompertz growth curves###
-#read in data
-setwd("C:/users/sfoerster/ABinteractions/Time_kill")
+########################################################################################################
+########################## Fitting the Gompertz Model to growth data####################################
+########################################################################################################
+
+#options
+
+
+#load packages
+library("cellGrowth", lib.loc="C:/Program Files/R/R-3.1.1/library")
+library("XLConnect")
+
+#read in the data
+setwd("C:/users/sfoerster/ABinteractions/Time_kill/GrowthCurves")
 curves=read.table("input/Growth_GW_short.txt",header=T)
-#calculate colony forming units from counts
+
+#calculate colony forming units from counts and make new data frame
 CFU <- curves$Count*curves$Dilution*100
 datatemp <- data.frame(curves$Strain,curves$Count,curves$Dilution,curves$Time,CFU,curves$Experiment)
 data <- data.frame(datatemp)
-
 names(data) <- c("strain","count","dilution","time","CFU","exp")
 strains <- sort(levels(data$strain))
-#data <- subset(data, exp == i,select=c(CFU, time,strain)) 
+regdata <- subset(data,data$time <= 20 & data$time >= 4)
 
+#Symbols and colors 
+colList<-c("grey39","grey","grey10")
+pchList <-c(21,24,23,NA)
+ltyList <-c(1,1,1,2)
+
+#initialize vectors
+parameters <- c()
+
+#fit model and calculate growth rate
 for (i in strains){
-  pdf(paste(i,"short.pdf",sep=""))
+  pdf(paste(i,".pdf",sep=""))
   y=data$CFU[data$strain==i]
-  print("-------------------------")
-  print(y)
   x=data$time[data$strain==i]
-  print(x)
   y=log(y)
-
+  
+  #fit linear regression from t4-t12
+  x1=regdata$time[regdata$strain==i]
+  y1=log(regdata$CFU[regdata$strain==i])
+  regression <- lm(y1~x1)
+  x_reg <- data.frame(x1=seq(from=-10,to=50,length.out=100))
+  y_reg <- predict(regression,x_reg,se.fit = TRUE)
+  
+  #guess sensible start parameters for Gompertz model
   guess <- guessCellGrowthParams(x,y,relative.height.at.lag=0.2)
   pars<-c(mu=guess$mu,l=guess$l,z0=guess$z0,zmax=guess$zmax)
-  #gomfit<-gompertz(x=x,mu=guess$mu,l=guess$l,z0=guess$z0,zmax=guess$zmax)
-  #fit2 <- optim(pars,ssr,gr=NULL,dat,hessian=TRUE)
   fit <- nls(y~gompertz(x,mu,l,z0,zmax),start=guess)
   print(fit)
   xnew=list(x=seq(min(x),max(x),length.out=1000))
   ypredict=exp(predict(fit,xnew))
-  plot(data$time[data$strain==i],data$CFU[data$strain==i],xlab="time[h]",cex.lab=1.3,ylab="bacteria[CFU/ml]",type="n",log="y",cex=1.5)
-  #points(x,y,col=c("black","red","green","blue"))
+  options("scipen"=100, "digits"=4)
+  plot(data$time[data$strain==i],data$CFU[data$strain==i],axes=FALSE, xlab = "", ylab = "",type="n",log="y",cex=1.5)
+  mtext(side = 1, text = "time[h]", cex=1.3,line = 2.5)
+  mtext(side = 2, text = expression("CFU [ml"^-1*"]"),cex=1.3, line = 2.5)
+  axis(1, c(0,4,8,12,16,20,24,28,32,36,40))
+  options("scipen"=-100, "digits"=4)
+  axis(2, c(1000,100000,10000000,1000000000))
+  box(lty = 1)
+
+ #plot points in different symbols for each experiment
   for(j in unique(data$exp)){
     newdata <- subset(data, exp == j& strain==i,select=c(CFU,time,strain,exp)) 
-    points(newdata$time,newdata$CFU,col=c(j+1),cex=1.5) 
+    points(newdata$time,newdata$CFU,pch=pchList[j],bg=colList[j],cex=1.7) 
   }
+  
+  # add regression line and gompertz fit
+  lines(x_reg$x1,exp(y_reg$fit),lty=2,lwd=2)
   lines(xnew$x,ypredict)
+  
+  # calculate parameters
+  growth=coef(summary(regression))[2]
   mu=round(coef(fit)[[1]],2)
   zmax=exp(round(coef(fit)[[4]],0))
   zmax=formatC(zmax,digits=2,format="E")
-  #legend("topleft",c(paste("growth rate [per hour]:",mu),paste("asymptote [CFU/ml]:",zmax)))
-  legend("topleft",c("Experiment 1","Experiment 2","Experiment 3"),pch=1,col=c(2,3,4),bty="n")
+  parameter=data.frame(i,mu,growth,zmax)
+  parameters=rbind(parameter,parameters)
+  # plot legend
+  legend("topleft",c("experiment 1","experiment 2","experiment 3","growth rate"),pch=pchList,lty=ltyList, pt.bg=colList, col="black",bty="n")
   dev.off()
 }
+names(parameters)<-c("strain","max. growth rate","growth rate","asymptote")
+allplots <- loadWorkbook("growth_parameter.xlsx", create = TRUE)  
+createSheet(allplots, name = "parameterlist")
+writeWorksheet(allplots, parameters, sheet = "parameterlist", startRow = 1, startCol = 1)
+saveWorkbook(allplots)
 
-
+#clean workspace
+#rm(list=ls())
+graphics.off()
 
